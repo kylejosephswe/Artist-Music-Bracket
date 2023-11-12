ac5c417c803bb2807418ecd5687c4d1249ed2d85
Kyle Joseph <kyle@Kyles-MacBook-Air.local>
2023-11-11 20:13:25 -0800

    Fixed the back button bug.

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,14 @@
-import Navbar from '../Components/Navbar'
-import ArtistSelection from '../Components/ArtistSelection'
+import Navbar from "../Components/Navbar";
+import ArtistSelection from "../Components/ArtistSelection";
 
 function Home() {
   return (
     <>
-    <h1 className="choose-artist">Choose an artist!</h1>
-    <ArtistSelection />
+      <h1 className="choose-artist">Artist Music Bracket</h1>
+      <h2 className="choose-artist">Choose an artist!</h2>
+      <ArtistSelection />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- src/Pages/Instructions.jsx
+++ src/Pages/Instructions.jsx
@@ -3,9 +3,8 @@ function Instructions() {
     <div className="instructions">
       <h1>1. Click an artist to play</h1>
       <h1>
-        2. Once you have an artist chosen, press the button of the song you like
-        more. Eliminate songs until there's only one song left aka your
-        favorite!
+        2. Once you have an artist chosen, press like on the song you like more.
+        Eliminate songs until there's only one song left aka your favorite!
       </h1>
     </div>
   );
